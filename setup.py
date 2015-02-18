b4055d4440f4955e7c9f75a47c101bf6c57a18a8
sheltonpaul89 <sheltonpaul89@gmail.com>
2015-02-18 19:27:32 +0530

    changes for distibution

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
   name = 'Web Mocker',
-  packages = ['web_mocker'], # this must be the same as the name above
+  packages = ['webmocker'], # this must be the same as the name above
   version = '0.1',
   description = 'A test lib for stubbing http response',
   author = 'Shelton Paul',
   author_email = 'sheltonpaul89@gmail.com',
-  url = 'https://github.com/sheltonpaul89/web_mocker', # use the URL to the github repo
+  url = 'https://github.com/sheltonpaul89/webmocker', # use the URL to the github repo
   download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
   keywords = ['web', 'stubbing', 'http','mock','Web Stubs'], # arbitrary keywords
   classifiers = [],
